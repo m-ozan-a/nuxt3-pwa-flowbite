5e8521173674930eb54ecb9593197eb16ff1a3d1
m-ozan-a <info@owuan.com>
2023-03-18 05:40:13 +0300

    change pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,50 +4,17 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         '@vueuse/nuxt',
         '@nuxtjs/tailwindcss',
-        '@vite-pwa/nuxt',
+        '@kevinmarrec/nuxt-pwa',
         'nuxt-icon'
     ],
     css: ['~/assets/css/main.css'],
     pwa: {
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Joyfoop PWA',
-        short_name: 'Joyfoop PWA',
-        start_url: '.',
-        display: 'standalone',
-        theme_color: '#ffffff',
-        description: 'Joyfoop PWA Flowbite starter kit.',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
+      meta: {
+        mobileAppIOS: true,
+      },      
       workbox: {
-        navigateFallback: '/',
-        globPatterns: ['**/*.{js,ts,vue,css,html,png,svg,ico}'],
-      },
-      client: {
-        installPrompt: true,
-        periodicSyncForUpdates: 20,
-      },
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      },
+        enabled: true
+      }
     },
     postcss: {
         plugins: {
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="space-2">
+        <h1>op</h1>
+    </div>
+</template>
